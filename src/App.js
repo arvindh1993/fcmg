55df734ae745c18c4653df711eb9eff829d19144
arvindh hd <arvindh1993@gmail.com>
2020-04-17 20:29:09 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -13,61 +13,95 @@ import {
   MDBBtn,
   MDBInput
 } from "mdbreact";
-function App() {
+
+
+
+
+export default class App extends React.Component{
+
+  state = {
+    value: 'Response Message'
+  };
+
+ handleClick() {
+ 
+  fetch('http://localhost:8000/')
+  .then(resp => resp.json())
+  .then((response) => {
+    this.setState({
+      value: response.resp
+    });
+
+  })
+  .catch((error)=>{
+    console.log(error);
+
+  })
+}
+render(){
+  
   return (
  
-<MDBContainer>
-<MDBRow>
-  <MDBCol md="6">
-    <MDBCard>
-      <MDBCardBody>
-        <MDBCardHeader className="form-header deep-blue-gradient rounded">
-          <h3 className="my-3">
-            <MDBIcon icon="lock" /> Covid 19 FCMG Helpline:
-          </h3>
-        </MDBCardHeader>
-        <form>
-          <div className="grey-text">
+    <MDBContainer>
+    <MDBRow>
+      <MDBCol md="6">
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardHeader className="form-header deep-blue-gradient rounded">
+              <h3 className="my-3">
+                <MDBIcon icon="lock" /> Covid 19 FCMG Helpline:
+              </h3>
+            </MDBCardHeader>
+            <form>
+              <div className="grey-text">
+                <MDBInput
+                  label="Type your email"
+                  icon="envelope"
+                  group
+                  type="email"
+                  validate
+                  error="wrong"
+                  success="right"
+                />
+                <MDBInput
+                  label="Type your Message"
+                  icon="lock"
+                  group
+                  type="password"
+                  validate
+                />
+              </div>
+    
+            <div className="text-center mt-4">
+              <MDBBtn
+                color="light-blue"
+                className="mb-3"
+                onClick={this.handleClick}
+              >
+                Submit
+              </MDBBtn>
+            </div>
+            </form>
+            <MDBModalFooter>
+              <div className="font-weight-light">
+              <a href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf">Covid 19 Helpline Numbers</a>
+               
+              </div>
+            </MDBModalFooter>
+            <div>
             <MDBInput
-              label="Type your email"
-              icon="envelope"
-              group
-              type="email"
-              validate
-              error="wrong"
-              success="right"
-            />
-            <MDBInput
-              label="Type your Message"
-              icon="lock"
-              group
-              type="password"
-              validate
-            />
+                  group
+                  type="text"
+                  value={this.state.value}
+                />
           </div>
-
-        <div className="text-center mt-4">
-          <MDBBtn
-            color="light-blue"
-            className="mb-3"
-            type="submit"
-          >
-            Submit
-          </MDBBtn>
-        </div>
-        </form>
-        <MDBModalFooter>
-          <div className="font-weight-light">
-          <a href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf">Covid 19 Helpline Numbers</a>
-          
-          </div>
-        </MDBModalFooter>
-      </MDBCardBody>
-    </MDBCard>
-  </MDBCol>
-</MDBRow>
-</MDBContainer>
-  );
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+    </MDBContainer>
+      );
+      
 }
 
-export default App;
+}
